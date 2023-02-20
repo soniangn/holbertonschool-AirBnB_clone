8030b06d04b94cd0297bcb51fa522238785216a6
TessierV <5628@holbertonstudents.com>
2023-02-20 04:31:56 -0800

    test script 3

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,2 +1,35 @@
 #!/usr/bin/python3
-"""Review"""
+"""Test class base"""
+
+import unittest
+from models.base import Base
+
+
+class testbase(unittest.TestCase):
+    """Class test base"""
+
+    """Tesst of Base"""
+    """Test of Base() for assigning automatically an ID exists"""
+    def test_id(self):
+        base = Base()
+        self.assertEqual(base.id, 1)
+
+    """Test of Base(89) saving the ID passed exists"""
+    def test_id_pass(self):
+        base = Base(89)
+        self.assertEqual(base.id, 89)
+
+    """Test of Base.to_json"""
+    """"All test validate from base.to_json"""
+    def test_none(self):
+        base = Base()
+        self.assertEqual(base.to_json_string([]), '[]')
+
+    """Base.from_json_string"""
+    """All test validate from Json string"""
+    def test_from_none(self):
+        self.assertEqual(Base.from_json_string('[]'), [])
+
+
+if __name__ == '__main__':
+    unittest.main()
