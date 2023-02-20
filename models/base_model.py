601efafb4ac1bdc0973f72d2c15a7e6d7ca95437
TessierV <5628@holbertonstudents.com>
2023-02-20 06:14:59 -0800

    script 4

--- models/base_model.py
+++ models/base_model.py
@@ -1,26 +1,53 @@
 #!/usr/bin/python3
-""" module for BaseModel """
+"""Model from BaseModel"""
 import uuid
 from datetime import datetime
 
+"""Create BaseModel"""
+
+
 class BaseModel:
-    """ defines all common attributes/methods for other classes """
-    def __init__(self, id=None, created_at=None, updated_at=None, name=None, my_number=0):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = updated_at
-        self.name = name
-        self.my_number = my_number
+    def __init__(self, *args, **kwargs):
+        """Assigns an argument to each attribute"""
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                format = "%Y-%m-%dT%H:%M:%S.%f"
+                if key == 'id':
+                    self.id = value
+                if key == 'created_at':
+                    self.created_at = datetime.strptime(value, format)
+                if key == 'my_number':
+                    self.my_number = value
+                if key == 'updated_at':
+                    self.updated_at = datetime.strptime(value, format)
+                if key == 'name':
+                    self.name = value
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(BaseModel.__name__, self.id, self.__dict__)
+        """String Doc"""
+        print_str = "[{}] ({}) {}"
+        return print_str.format(BaseModel.__name__, self.id, self.__dict__)
 
     def save(self):
+        """Save the documentation"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        dictionary = dict(id=self.id, my_number=self.my_number, name=self.name, updated_at=self.updated_at, created_at=self.created_at)
-        dictionary.update({'__class__': BaseModel.__name__})
-        dictionary.update({'created_at': self.created_at.isoformat()})
-        dictionary.update({'updated_at': self.updated_at.isoformat()})
-        return dictionary
+        """Return the Dictionnary """
+        dic = {}
+        dic["id"] = self.id
+        dic["created_at"] = self.created_at
+        dic["my_numbers"] = self.my_number
+        dic["updated_at"] = self.updated_at
+        dic["name"] = self.name
+
+        dic.update({'__class__': BaseModel.__name__})
+        dic.update({'created_at': self.created_at.isoformat()})
+        dic.update({'updated_at': self.updated_at.isoformat()})
+        return dic
