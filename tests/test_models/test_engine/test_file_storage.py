bb9d8a5411f74623a78c147c621019dd47af7766
TessierV <5628@holbertonstudents.com>
2023-02-21 03:14:33 -0800

    test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,2 +1,45 @@
 #!/usr/bin/python3
-"""Review"""
+"""Test class base"""
+
+import unittest
+from models.base_model import BaseModel
+
+
+class FileStorage(unittest.TestCase):
+    """Class test FileStorage"""
+
+    """Test of __file_path:"""
+    def test_file_path(self):
+        file = FileStorage()
+        self.assertEqual(file._FileStorage__file_path, "file.json")
+
+    """Test of __objects"""
+    """def test_objects(self):
+        file = FileStorage()"""
+
+    """Test of all()"""
+    """def test_all(self):
+        FileStorage.all"""
+
+
+    """Test of new()"""
+    """def test_new(self):
+        FileStorage.new()"""
+
+
+    """Test of save()"""
+    """def test_safe(self):
+        FileStorage.safe()"""
+
+
+    """Test of reload()"""
+    """def test_reload(self):
+        with self.assertRaises(TypeError):
+            FileStorage.reload(None)
+
+    def test_objects(self):
+        self.assertEqual(dict, type(FileStorage._FileStorage__objects))"""
+
+
+if __name__ == '__main__':
+    unittest.main()
