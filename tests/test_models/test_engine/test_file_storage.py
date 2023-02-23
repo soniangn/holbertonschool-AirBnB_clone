10753ee902e1180aec877e4cde5650b220ed56b5
TessierV <5628@holbertonstudents.com>
2023-02-23 05:52:30 -0800

    test filestorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
-import unittest
 from models.engine.file_storage import FileStorage
+from models import storage
 from models.base_model import BaseModel
-
+import pep8
+import unittest
+import json
 
 class TestFileStorage(unittest.TestCase):
-    """Testing FileStorage"""
-    def test_filestorage(self):
-        storage = FileStorage()
-        storage.all().clear()
-        storage.reload()
-        self.assertFalse(len(storage.all()) == 0)
+
+    def test__file_path(self):
+        obj = FileStorage()
+        self.assertEqual(obj._FileStorage__file_path, "file.json")
+
+    def test_objects(self):
+        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
+
+    def test_reload(self):
+        with self.assertRaises(TypeError):
+            storage.reload(None)
+
+    def test_all(self):
+        self.assertEqual(FileStorage._FileStorage__objects, storage.all())
+
+    def test_save(self):
+        with open(FileStorage._FileStorage__file_path, 'r') as f:
+            self.assertEqual(dict, type(json.load(f)))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
