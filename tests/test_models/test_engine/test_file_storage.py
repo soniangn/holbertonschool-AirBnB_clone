9a2bc13ceedfc292bab7314f5561f68f0092b45c
TessierV <5628@holbertonstudents.com>
2023-02-23 04:33:48 -0800

    git pull

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,6 +5,7 @@ import unittest
 from models.base_model import BaseModel
 from models.user import User
 from models.engine.file_storage import FileStorage
+import os
 
 
 class TestFileStorage(unittest.TestCase):
