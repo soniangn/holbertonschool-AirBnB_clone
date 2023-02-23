25818c10f554518c9ad6171bf7aabd41c01beeb0
TessierV <5628@holbertonstudents.com>
2023-02-23 04:25:32 -0800

    test amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -8,26 +8,9 @@ from models.amenity import Amenity
 
 class TestAmenity(unittest.TestCase):
     """ test Amenity """
-    def setUp(self):
-        """ creates an instance of Amenity """
-        self.amenity = Amenity()
-        self.amenity.name = "air-conditioning"
-
-    @classmethod
-    def tearDownClass(cls):
-        """ delete json file at the end of tests """
-        try:
-            os.remove("file.json")
-        except Exception:
-            pass
-
     def test_city(self):
         """ checks city's attributes """
-        self.assertEqual(self.amenity.name, "air-conditioning")
-
-    def test_subclass(self):
-        """ checks if Amenity is a subclass of BaseModel """
-        self.assertEqual(True, issubclass(Amenity, BaseModel))
+        self.assertEqual(Amenity.name, "")
 
 
 if __name__ == '__main__':
