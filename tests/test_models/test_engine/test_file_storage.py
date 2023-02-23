953790e66906579ecc64eb6848d5da0c2651c4cf
TessierV <5628@holbertonstudents.com>
2023-02-23 05:55:40 -0800

    test filestorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,6 +3,7 @@
 import unittest
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+import pep8
 
 
 class TestFileStorage(unittest.TestCase):
@@ -12,6 +13,13 @@ class TestFileStorage(unittest.TestCase):
         storage.all().clear()
         storage.reload()
         self.assertFalse(len(storage.all()) == 0)
+
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/engine/file_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
 if __name__ == "__main__":
     unittest.main()
