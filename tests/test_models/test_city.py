31e0701503c59c5c368636560b2883d65b4b2f9a
TessierV <5628@holbertonstudents.com>
2023-02-23 03:52:17 -0800

    test city

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -8,14 +8,12 @@ import os
 
 
 class TestCity(unittest.TestCase):
-    """ test City """
-    def setUp(self):
+
+    def test_city(self):
         """ creates an instance of City """
         self.city = City()
         self.city.state_id = "31000"
         self.city.name = "Toulouse"
-
-    def test_city(self):
         """ checks city's attributes """
         self.assertEqual(self.city.state_id, "31000")
         self.assertEqual(self.city.name, "Toulouse")
