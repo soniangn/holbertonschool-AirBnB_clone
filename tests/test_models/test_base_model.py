e1a7b01a7145f9353c1340979333643a29a3e063
TessierV <5628@holbertonstudents.com>
2023-02-23 01:28:07 -0800

    modificate import variable

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -9,24 +9,24 @@ class testbase(unittest.TestCase):
     """Tesst of Base"""
     """Test of Base() for assigning automatically an ID exists"""
     def test_id(self):
-        base = Base()
+        base = BaseModel()
         self.assertEqual(base.id, 1)
 
     """Test of Base(89) saving the ID passed exists"""
     def test_id_pass(self):
-        base = Base(89)
+        base = BaseModel(89)
         self.assertEqual(base.id, 89)
 
     """Test of Base.to_json"""
     """All test validate from base.to_json"""
     def test_none(self):
-        base = Base()
+        base = BaseModel()
         self.assertEqual(base.to_json_string([]), '[]')
 
     """Base.from_json_string"""
     """All test validate from Json string"""
     def test_from_none(self):
-        self.assertEqual(Base.from_json_string('[]'), [])
+        self.assertEqual(BaseModel.from_json_string('[]'), [])
 
 
 if __name__ == '__main__':
