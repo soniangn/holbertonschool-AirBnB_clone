8ecc517021355904afde4e61102954e3d0e6f207
TessierV <5628@holbertonstudents.com>
2023-02-23 05:31:22 -0800

    test filestorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-"""
-Unit tests for Base class
-"""
-import unittest
-from models import storage
-import pep8
-from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+from models import storage
+from models.base_model import BaseModel
+import models
+import unittest
+import json
 
 
-
-class Test_Base(unittest.TestCase):
-    """Base class tests"""
+class TestFileStorage(unittest.TestCase):
 
     def test__file_path(self):
         obj = FileStorage()
@@ -26,6 +22,17 @@ class Test_Base(unittest.TestCase):
 
     def test_all(self):
         self.assertEqual(FileStorage._FileStorage__objects, storage.all())
+
+    def test_save(self):
+        with open(FileStorage._FileStorage__file_path, 'r') as f:
+            self.assertEqual(dict, type(json.load(f)))
+
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/engine/file_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
 if __name__ == '__main__':
     unittest.main()
