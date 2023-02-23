d5a89dafae585cf91670fb0585405809018cddae
soniangn <nguyen.sonia@gmail.com>
2023-02-23 05:39:13 -0800

    added pep8 test in test base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -50,12 +50,5 @@ class TestBaseModel(unittest.TestCase):
         base.save()
         self.assertEqual(dict, type(storage.all))
 
-    def test_pep8_conformance(self):
-        """Test that we conform to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/engine/file_storage.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-if __name__ == '__main__':
+    if __name__ == '__main__':
     unittest.main()
