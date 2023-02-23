f40b6d52c44c59a5d93449dac4f2c52c25715487
TessierV <5628@holbertonstudents.com>
2023-02-23 04:00:21 -0800

    test user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -10,20 +10,13 @@ import os
 
 class TestUser(unittest.TestCase):
     """ test User """
-    def setUp(self):
-        """ creates an instance of User with attributes """
-        self.user = User()
-        self.user.first_name = "Vanessa"
-        self.user.last_name = "Tessier"
-        self.user.password = "sTr@n_G"
-        self.user.email = "vt@mail.com"
 
     def test_user(self):
         """ checks user's attributes """
-        self.assertEqual(str, type(self.user.first_name))
-        self.assertEqual(str, type(self.user.last_name))
-        self.assertEqual(str, type(self.user.password))
-        self.assertEqual(str, type(self.user.email))
+        self.assertEqual(str, type(User.first_name))
+        self.assertEqual(str, type(User.last_name))
+        self.assertEqual(str, type(User.password))
+        self.assertEqual(str, type(User.email))
 
 
 if __name__ == '__main__':
