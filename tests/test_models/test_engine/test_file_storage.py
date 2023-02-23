fffda380acd5c82e75816c9e14a7670c7dd7c253
TessierV <5628@holbertonstudents.com>
2023-02-23 05:29:25 -0800

    test filestorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,17 +6,26 @@ import unittest
 from models import storage
 import pep8
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
 
 
 class Test_Base(unittest.TestCase):
     """Base class tests"""
 
-    def test(self):
-        """  Test Dictionary """
-        model = BaseModel()
-        model.save()
-        new_object = storage.all()
-        self.assertEqual(dict, type(new_object))
+    def test__file_path(self):
+        obj = FileStorage()
+        self.assertEqual(obj._FileStorage__file_path, "file.json")
+
+    def test_objects(self):
+        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
+
+    def test_reload(self):
+        with self.assertRaises(TypeError):
+            storage.reload(None)
+
+    def test_all(self):
+        self.assertEqual(FileStorage._FileStorage__objects, storage.all())
 
     def test_pep8_conformance(self):
         """Test that we conform to PEP8."""
