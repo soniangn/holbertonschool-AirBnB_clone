06e0167aeb756ebf3c0b845de45aaf1c3c2eee9f
TessierV <5628@holbertonstudents.com>
2023-02-23 05:23:11 -0800

    test filestorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,36 +5,25 @@ Unit tests for Base class
 import unittest
 from models import storage
 import pep8
-import json
 from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
+
 
 class Test_Base(unittest.TestCase):
     """Base class tests"""
 
-    def test__file_path(self):
-        self.assertEqual(FileStorage._FileStorage__file_path, "file.json")
+    def test(self):
+        """  Test Dictionary """
+        model = BaseModel()
+        model.save()
+        new_object = storage.all()
+        self.assertEqual(dict, type(new_object))
 
-    def test_objects(self):
-        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
-
-    def test_reload(self):
-        with self.assertRaises(TypeError):
-            storage.reload(None)
-
-    def test_all(self):
-        self.assertEqual(FileStorage._FileStorage__objects, storage.all())
-
-    def test_save(self):
-        with open(FileStorage._FileStorage__file_path, 'r') as f:
-            self.assertEqual(dict, type(json.load(f)))
-
-    def pep8_conformance(self):
+    def test_pep8_conformance(self):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['models/engine/file_storage.py'])
-        print = "Found code style errors"
-        self.assertEqual(result.total_errors, 0, print)
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
 if __name__ == '__main__':
     unittest.main()
