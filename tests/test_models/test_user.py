f049778f2ca532511545b39a70977d780243bb57
TessierV <5628@holbertonstudents.com>
2023-02-23 03:26:12 -0800

    test

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -10,28 +10,13 @@ import os
 
 class TestUser(unittest.TestCase):
     """ test User """
-    def setUp(self):
-        """ creates an instance of User with attributes """
-        self.user = User()
-        self.user.first_name = "Vanessa"
-        self.user.last_name = "Tessier"
-        self.user.password = "sTr@n_G"
-        self.user.email = "vt@mail.com"
-
-    @classmethod
-    def tearDownClass(cls):
-        """ delete json file at the end of tests """
-        try:
-            os.remove("file.json")
-        except Exception:
-            pass
 
     def test_user(self):
         """ checks user's attributes """
-        self.assertEqual(str, type(self.user.first_name), "Vanessa")
-        self.assertEqual(str, type(self.user.last_name), "Tessier")
-        self.assertEqual(str, type(self.user.password), "sTr@n_G")
-        self.assertEqual(str, type(self.user.email), "vt@mail.com")
+        self.assertEqual(str, type(self.user.first_name))
+        self.assertEqual(str, type(self.user.last_name))
+        self.assertEqual(str, type(self.user.password))
+        self.assertEqual(str, type(self.user.email))
 
 
 if __name__ == '__main__':
