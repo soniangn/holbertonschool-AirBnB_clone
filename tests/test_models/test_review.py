78224bd5cd9e4111fcfb8867e7be160ecf86614e
TessierV <5628@holbertonstudents.com>
2023-02-23 04:16:03 -0800

    test review

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -8,29 +8,12 @@ from models.review import Review
 
 class TestReview(unittest.TestCase):
     """ test Review """
-    def setUp(self):
-        """ creates an instance of Review """
-        self.review = Review()
-        self.review.place_id = "31000"
-        self.review.user_id = "Ch@arlie5433"
-        self.review.text = "Great experience"
-
-    @classmethod
-    def tearDownClass(cls):
-        """ delete json file at the end of tests """
-        try:
-            os.remove("file.json")
-        except Exception:
-            pass
 
     def test_review(self):
         """ checks review's attributes """
-        self.assertEqual(self.review.place_id, "31000")
-        self.assertEqual(self.review.user_id, "Ch@arlie5433")
-        self.assertEqual(self.review.text, "Great experience")
-
-    def test_subclass(self):
-        """ checks if Review is a subclass of BaseModel """
+        self.assertEqual(Review.place_id, '')
+        self.assertEqual(Review.user_id, '')
+        self.assertEqual(Review.text, '')
         self.assertEqual(True, issubclass(Review, BaseModel))
 
 if __name__ == '__main__':
