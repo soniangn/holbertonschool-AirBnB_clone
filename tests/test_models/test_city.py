76d47e898bbbb33fabe9fcce85b219f907db9e40
TessierV <5628@holbertonstudents.com>
2023-02-23 03:32:29 -0800

    test city

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -9,27 +9,10 @@ import os
 
 class TestCity(unittest.TestCase):
     """ test City """
-    def setUp(self):
-        """ creates an instance of City """
-        self.city = City()
-        self.city.state_id = "31000"
-        self.city.name = "Toulouse"
-
-    @classmethod
-    def tearDownClass(cls):
-        """ delete json file at the end of tests """
-        try:
-            os.remove("file.json")
-        except Exception:
-            pass
-
     def test_city(self):
         """ checks city's attributes """
         self.assertEqual(self.city.state_id, "31000")
         self.assertEqual(self.city.name, "Toulouse")
-
-    def test_subclass(self):
-        """ checks if City is a subclass of BaseModel """
         self.assertEqual(True, issubclass(City, BaseModel))
 
 if __name__ == '__main__':
