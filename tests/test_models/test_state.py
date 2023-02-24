77caec1630d4d8b75cb8272788fbca1e19375b54
soniangn <nguyen.sonia@gmail.com>
2023-02-24 01:42:03 -0800

    added comments to test_state

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Review"""
-""" Unittest for State """
+""" Module for State's unit tests """
 import unittest
 from models.base_model import BaseModel
 from models.state import State
@@ -10,8 +9,8 @@ import os
 class TestState(unittest.TestCase):
     """ test State """
 
-
     def test_state(self):
+        """ checks State's attribute """
         self.assertEqual(State.name, '')
 
 if __name__ == '__main__':
