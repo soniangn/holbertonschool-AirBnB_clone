d2e109bb6e819d37c604415b8d92d8a12d7cbd73
soniangn <nguyen.sonia@gmail.com>
2023-02-24 01:41:05 -0800

    added comments to test_review

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Review"""
-""" Unittest for Review """
+""" Module for Review unit tests """
 import unittest
 from models.base_model import BaseModel
 from models.review import Review
